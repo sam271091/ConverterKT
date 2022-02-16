30cfa3b809402c485067ed6b6613b97d20f19b81
Samir <sam271091@gmail.com>
2022-02-16 21:05:39 +0400

    First commit

---
+++ app/src/main/java/Apps/com/converterkt/SplashActivity.kt
@@ -0,0 +1,50 @@
+package Apps.com.converterkt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.concurrent.TimeUnit
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+
+        with(animationView){
+            speed = 3f
+        }
+
+
+//        val handler = Handler(Looper.getMainLooper())
+//        handler.postDelayed({
+//            startActivity(intent)
+//            finish()
+//        }, 3500)
+
+
+
+
+        val disposable = Observable.timer(3500, TimeUnit.MILLISECONDS)
+            .subscribe {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+
+
+//        disposable.dispose()
+
+    }
+
+
+}
